cb4e05f57877034aa5d9e99de11460e9404807ef
shareifjones <shareifjones@gmail.com>
2024-07-05 09:55:29 -0400

    made updates for 3.4

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+
+export const MainView = () => {
+    const [movies, setMovies] = useState([
+    ]);
+    const [selectedMovie, setSelectedMovie] = useState(null);
+
+    useEffect(() => {
+        fetch("https://shareif-flix-0b8cde79839e.herokuapp.com/movies")
+            .then((response) => response.json())
+            .then((data) => {
+                const moviesFromApi = data.map(movie => {
+                    return {
+                        id: movie._id,
+                        title: movie.Title,
+                        description: movie.Description,
+                        genre: [{
+                            name: movie.Genre.Name,
+                            description: movie.Genre.Description
+                        }],
+                        director: [{
+                            name: movie.Director.Name,
+                            bio: movie.Director.Bio,
+                            birth: movie.Director.Birth
+                        }],
+                        image: movie.Image,
+                        featured: movie.Featured
+                    }
+                });
+
+                setMovies(moviesFromApi);
+            });
+    }, []);
+
+    if (selectedMovie) {
+        return <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />;
+    }
+
+    if (movies.length === 0) {
+        return <div>The list is empty!</div>;
+    }
+
+    return (
+        <div>
+            {movies.map((movie) => (
+                <MovieCard
+                    key={movie.id}
+                    movie={movie}
+                    onMovieClick={(newSelectedMovie) => {
+                        setSelectedMovie(newSelectedMovie);
+                    }}
+                />
+            ))}
+        </div>
+    );
+}
