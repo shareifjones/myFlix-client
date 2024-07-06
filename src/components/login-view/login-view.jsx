e5507e0a09cba7c77d2b7e59db16635b6fd28ee6
shareifjones <shareifjones@gmail.com>
2024-07-05 21:30:33 -0400

    updated for 3.5

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,15 +1,57 @@
+import React, { useState } from "react";
+
 export const LoginView = () => {
-    return (
-        <form>
-            <label>
-                Username:
-                <input type="text" />
-            </label>
-            <label>
-                Password:
-                <input type="password" />
-            </label>
-            <button type="submit">Submit</button>
-        </form>
-    )
-}
+    const [username, setUsername] = useState("");
+    const handleSubmit = (event) => {
+        // this prevents the default behavior of the form which is to reload the entire page
+        event.preventDefault();
+
+        const data = {
+            access: username,
+            secret: password
+        };
+
+        fetch("https://shareif-flix-0b8cde79839e.herokuapp.com/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("Login response: ", data);
+                if (data.user) {
+                    localStorage.setItem("user", JSON.stringify(data.user));
+                    localStorage.setItem("token", data.token);
+                    onLoggedIn(data.user, data.token);
+                } else {
+                    alert("No such user");
+                }
+            })
+            .catch((e) => {
+                alert("Something went wrong");
+            });
+    }
+};
+
+<form onSubmit={handleSubmit}>
+    <label>
+        Username:
+        <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required />
+    </label>
+    <label>
+        Password:
+        <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required />
+    </label>
+    <button type="submit">Submit</button>
+</form>
+
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,58 +1,85 @@
 import { useEffect, useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
+
 
 export const MainView = () => {
-    const [movies, setMovies] = useState([
-    ]);
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedToken = localStorage.getItem("token");
+    const [user, setUser] = useState(storedUser ? storedUser : null);
+    const [token, setToken] = useState(storedToken ? storedToken : null);
+    const [movies, setMovies] = useState([]);
     const [selectedMovie, setSelectedMovie] = useState(null);
 
+
     useEffect(() => {
-        fetch("https://shareif-flix-0b8cde79839e.herokuapp.com/movies")
+        if (!token) {
+            return;
+        }
+        fetch("https://shareif-flix-0b8cde79839e.herokuapp.com/movies", {
+            headers: { Authorization: `Bearer ${token}` },
+        })
             .then((response) => response.json())
-            .then((data) => {
-                const moviesFromApi = data.map(movie => {
-                    return {
-                        id: movie._id,
-                        title: movie.Title,
-                        description: movie.Description,
-                        genre: [{
-                            name: movie.Genre.Name,
-                            description: movie.Genre.Description
-                        }],
-                        director: [{
-                            name: movie.Director.Name,
-                            bio: movie.Director.Bio,
-                            birth: movie.Director.Birth
-                        }],
-                        image: movie.Image,
-                        featured: movie.Featured
-                    }
-                });
-
-                setMovies(moviesFromApi);
+            .then((movies) => {
+                setMovies(movies);
             });
-    }, []);
+    }, [token]);
 
-    if (selectedMovie) {
-        return <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />;
-    }
+    const moviesFromApi = data.map(movie => {
+        return {
+            id: movie._id,
+            title: movie.Title,
+            description: movie.Description,
+            genre: [{
+                name: movie.Genre.Name,
+                description: movie.Genre.Description
+            }],
+            director: [{
+                name: movie.Director.Name,
+                bio: movie.Director.Bio,
+                birth: movie.Director.Birth
+            }],
+            image: movie.Image,
+            featured: movie.Featured
+        }
+    });
+}
+setMovies(moviesFromApi);
 
-    if (movies.length === 0) {
-        return <div>The list is empty!</div>;
-    }
-
+if (!user) {
     return (
-        <div>
-            {movies.map((movie) => (
-                <MovieCard
-                    key={movie.id}
-                    movie={movie}
-                    onMovieClick={(newSelectedMovie) => {
-                        setSelectedMovie(newSelectedMovie);
-                    }}
-                />
-            ))}
-        </div>
+        <>
+            <LoginView onLoggedIn={(user, token) => {
+                setUser(user);
+                setToken(token);
+            }} />
+            or
+            <SignupView />
+        </>
     );
 }
+
+if (selectedMovie) {
+    return <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />;
+}
+
+if (movies.length === 0) {
+    return <div>The list is empty!</div>;
+}
+
+return (
+    <div>
+        <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
+        {movies.map((movie) => (
+            <MovieCard
+                key={movie.id}
+                movie={movie}
+                onMovieClick={(newSelectedMovie) => {
+                    setSelectedMovie(newSelectedMovie);
+                }}
+            />
+        ))}
+    </div>
+);
