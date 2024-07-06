74081967aa5b29545531d498bcf5a0209c315db2
shareifjones <shareifjones@gmail.com>
2024-07-05 21:49:54 -0400

    made update for 3.4

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
         <div
-            onMovieClick={() => {
+            onClick={() => {
                 onMovieClick(movie);
             }}
         >
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { createRoot } from 'react-dom/client';
 import { MainView } from './components/main-view/main-view';
 
